2975cbfe779d51a4592fd50991a6f8f00a1664a3
uvejs <hotiu@bzz.ch>
2024-12-02 14:37:06 +0100

    done

---
+++ test_lottery.py
@@ -0,0 +1,17 @@
+"""test_lottery"""
+from lottery import create_ticket
+from person import Person
+
+def test_create_ticket_success(monkeypatch, capsys):
+    person = Person("Max", "geheim", 10.00)
+    inputs = iter(["1", "2", "3", "4", "5", "6", "3"])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    create_ticket(person)
+    output = capsys.readouterr().out
+    assert "Dein neues Guthaben: 8.00" in output
+
+def test_create_ticket_fail(capsys):
+    person = Person("Max", "geheim", 1.00)
+    create_ticket(person)
+    output = capsys.readouterr().out
+    assert "Zuwenig Guthaben" in output
---
+++ test_money.py
@@ -0,0 +1,10 @@
+"""test_money"""
+from money import transfer_money
+from person import Person
+
+def test_transfer_money_success(monkeypatch):
+    person = Person("Max", "geheim", 50.00)
+    inputs = iter(["E", "10", "Z"])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    transfer_money(person)
+    assert person.balance == 60.00
